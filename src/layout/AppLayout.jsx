e5fc3f34a48c5efac613df3baec9fc1c1a6730ff
Didierson AMURI <didiamuri@gmail.com>
2021-10-21 14:18:52 +0100

    update

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from '../components/Home';
 import MainQuizPage from '../components/quiz-page/Main';
+import Footer from '../components/shared/Footer';
 import Header from '../components/shared/Header';
 import MainSubjectPage from '../components/subject-page/Main';
 import MainVideoPage from '../components/video-page/Main';
@@ -17,6 +18,7 @@ const AppLayout = () => {
                 <Route exact path="/app/video" component={MainVideoPage} />
                 <Route exact path="/app/quiz" component={MainQuizPage} />
             </Switch>
+            <Footer />
         </Router>
     )
 }
